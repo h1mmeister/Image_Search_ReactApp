1df636abf34e8f430c1a39d3248415aa83e64c1c
h1m <himanshu.yadav643@gmail.com>
2020-12-27 06:45:23 +0530

    completed till API invocation

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,10 +2,15 @@ import React from "react";
 
 class SeachBar extends React.Component {
   state = { term: "" };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.term);
+  };
   render() {
     return (
       <div className="ui segemnt">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
             <input
